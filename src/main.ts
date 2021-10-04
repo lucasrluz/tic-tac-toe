d2b4c1b4f6860b4f1b773428a2f400a92c2fde46
lucasrluz <lucasr.luzbr@gmail.com>
2021-10-04 20:21:20 -0300

    Loop que fica alterando a vez de cada jogador

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
-import { setFirstPlayer } from "./set-first-player";
+import { question } from "./interface/readline";
+import { renderGameBoard } from "./game/render-game-board";
+import { setFirstPlayer } from "./input/set-first-player";
+import { validateWinner } from "./service/validate-winner";
+import { setPosition } from "./input/set-position";
+import { IPlayer } from "./interface/player-interface";
 
 async function main() {
   const players = await setFirstPlayer();
 
   let round = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
+
+  let player: IPlayer;
+  let i = true;
+  while (true) {
+    player = i ? players[0] : players[1];
+    i = !i;
+  }
 }
 main();
