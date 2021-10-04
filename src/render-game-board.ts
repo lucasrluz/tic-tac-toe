a5a0b734ab265d9200971f0d95d2f910b46cfd07
lucasrluz <lucasr.luzbr@gmail.com>
2021-10-04 09:41:25 -0300

    Função que limpa a tela e depois mostra o tabuleiro do jogo

--- src/render-game-board.ts
+++ src/render-game-board.ts
@@ -1,5 +1,7 @@
 export function renderGameBoard(round: string[]) {
-  return `
+  process.stdout.write('\u001b[H\u001b[2J\u001b[3J');
+  console.log(
+  `
 + - + - + - +
 | ${round[0]} | ${round[1]} | ${round[2]} |
 + - + - + - +
@@ -8,4 +10,5 @@ export function renderGameBoard(round: string[]) {
 | ${round[6]} | ${round[7]} | ${round[8]} |
 + - + - + - +
   `
+  )
 }
