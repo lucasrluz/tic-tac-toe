f4c6f310ae02723be540118bc95dc1ec524109e4
lucasrluz <lucasr.luzbr@gmail.com>
2021-10-04 20:13:48 -0300

    Função que verifica se existe um ganhador

---
+++ src/service/validate-winner.ts
@@ -0,0 +1,16 @@
+import { IPlayer } from "../interface/player-interface";
+
+export function validateWinner(player: IPlayer, round: string[]) {
+  if (
+    (round[0] === round[1] && round[0] === round[2] && round[0] !== ' ') ||
+    (round[0] === round[3] && round[0] === round[6] && round[0] !== ' ') ||
+    (round[0] === round[4] && round[0] === round[8] && round[0] !== ' ') ||
+    (round[1] === round[4] && round[1] === round[7] && round[1] !== ' ') ||
+    (round[2] === round[4] && round[2] === round[6] && round[2] !== ' ') ||
+    (round[2] === round[5] && round[2] === round[8] && round[2] !== ' ') ||
+    (round[3] === round[4] && round[3] === round[5] && round[3] !== ' ') ||
+    (round[6] === round[7] && round[6] === round[8] && round[6] !== ' ')
+  ) return player;
+
+  return false;
+}
