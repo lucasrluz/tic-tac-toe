3e4c4b56f02052148e91781d633450f4a2f0cd76
lucasrluz <lucasr.luzbr@gmail.com>
2022-01-04 09:40:59 -0300

    remoção da interface readline

--- src/interface/readline.ts
+++
@@ -1,9 +0,0 @@
-import readline from 'readline';
-import util from 'util';
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-export const question = util.promisify(rl.question).bind(rl);
