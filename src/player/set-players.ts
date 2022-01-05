92dc3875c2011aa77efa595a306f072bd50a53da
lucasrluz <lucasr.luzbr@gmail.com>
2022-01-05 16:31:43 -0300

    alteração no diretório e refatoração para setar jogadores

---
+++ src/player/set-players.ts
@@ -0,0 +1,18 @@
+import { IPlayer } from '../interface/player-interface';
+import { setPlayOrder } from '../input/setPlayOrder';
+
+export async function setPlayers() {
+  const players: IPlayer[] = [{ symbol: '' }, { symbol: '' }];
+
+  process.stdout.write('\u001b[H\u001b[2J\u001b[3J');
+
+  const { firstPlayer } = await setPlayOrder();
+
+  players[0].symbol = firstPlayer;
+
+  players[0].symbol === 'x'
+    ? (players[1].symbol = 'o')
+    : (players[1].symbol = 'x');
+
+  return players;
+}
