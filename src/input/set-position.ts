4317f7e7095228af474aac72001391ff2b2ab003
lucasrluz <lucasr.luzbr@gmail.com>
2022-01-05 16:26:29 -0300

    alteração no tipo de input para setar posição de jogada

--- src/input/set-position.ts
+++ src/input/set-position.ts
@@ -1,14 +1,15 @@
 import inquirer from 'inquirer';
 import { IPlayer } from '../interface/player-interface';
 import { validateMove } from '../service/validate-move';
+import { validateInput } from '../service/validateInput';
 
 export async function setPosition(round: string[], player: IPlayer) {
   const { position } = await inquirer.prompt([
     {
-      type: 'list',
+      type: 'input',
       name: 'position',
-      message: `Em qual posição o jogador ${player.id}(${player.symbol}) quer jogar?\n:`,
-      choices: ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
+      message: `Em qual posição o jogador (${player.symbol}) quer jogar?\n:`,
+      validate: validateInput,
     },
   ]);
 
