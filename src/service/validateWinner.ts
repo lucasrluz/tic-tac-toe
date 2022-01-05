654718e4e4ee3611d85ced8bc8ed4ab9924a65ef
lucasrluz <lucasr.luzbr@gmail.com>
2022-01-05 20:24:45 -0300

    alteração na regra de negócio para verificar se existe um ganhador

--- src/service/validateWinner.ts
+++ src/service/validateWinner.ts
@@ -2,14 +2,14 @@ import { IPlayer } from '../interface/IPlayer';
 
 export function validateWinner(player: IPlayer, round: string[]) {
   if (
-    (round[0] === round[1] && round[0] === round[2] && round[0] !== ' ') ||
-    (round[0] === round[3] && round[0] === round[6] && round[0] !== ' ') ||
-    (round[0] === round[4] && round[0] === round[8] && round[0] !== ' ') ||
-    (round[1] === round[4] && round[1] === round[7] && round[1] !== ' ') ||
-    (round[2] === round[4] && round[2] === round[6] && round[2] !== ' ') ||
-    (round[2] === round[5] && round[2] === round[8] && round[2] !== ' ') ||
-    (round[3] === round[4] && round[3] === round[5] && round[3] !== ' ') ||
-    (round[6] === round[7] && round[6] === round[8] && round[6] !== ' ')
+    (round[0] === round[1] && round[0] === round[2]) ||
+    (round[0] === round[3] && round[0] === round[6]) ||
+    (round[0] === round[4] && round[0] === round[8]) ||
+    (round[1] === round[4] && round[1] === round[7]) ||
+    (round[2] === round[4] && round[2] === round[6]) ||
+    (round[2] === round[5] && round[2] === round[8]) ||
+    (round[3] === round[4] && round[3] === round[5]) ||
+    (round[6] === round[7] && round[6] === round[8])
   )
     return player;
 
